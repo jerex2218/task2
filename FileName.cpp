4762ee2aba07417c086a931de3da2aec8966fd1d
jerex2218 <tprivet92@gmail.com>
2025-03-27 22:44:33 +0000

    translate comments in FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-//начало кода
+//start of the code
 int main() {
-    std::string name;                // тут имя
-    std::cout << "Enter your name: "; //тут спрашиваем это имя
-    std::cin >> name;                  //узнаём имя
-    std::cout << "Hello world from " << name << std::endl; //пишем имя
+    std::string name;                // there is name
+    std::cout << "Enter your name: "; //asking for the name
+    std::cin >> name;                  //reading the name
+    std::cout << "Hello world from " << name << std::endl; //printing the name
     return 0;
-}//конец кода
+}//end
